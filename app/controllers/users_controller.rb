d40eab9ce0ca58d736bbefb71747afb1bd506ad6
EC2 Default User <ec2-user@ip-172-31-19-58.ec2.internal>
2022-02-24 06:39:54 +0000

    [Update]Bookers2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @books = @user.books
     @book = Book.new
+    @post_image = @user.post_images.page(params[:id])
   end
 
   def edit
